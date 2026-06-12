d1fa4229f651033819798a2da56fee432a821959
marshall-hash <71224700+marshall-hash@users.noreply.github.com>
2026-06-12 14:54:27 +0200

    FINAL 2

--- backend/routes/fileUpload.js
+++ backend/routes/fileUpload.js
@@ -181,7 +181,8 @@ catch (error) {
 router.get('/download', async (req, res) => {
   try {
     const browser = await puppeteer.launch({
-      executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+
+      executablePath:  process.env.PUPPETEER_EXECUTABLE_PATH || 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
       headless: true, 
     });
 
--- main.js
+++ main.js
@@ -1,33 +1,37 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const { fork } = require('child_process');
 
-const { spawn } = require('child_process');
+app.setPath(
+  'userData',
+  path.join(app.getPath('appData'), 'reports-generator')
+);
 
-app.setPath('userData', path.join(app.getPath('appData'), 'reports-generator'));
 app.commandLine.appendSwitch('disable-gpu');
+
 let backendProcess;
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 1400,
-    height: 900
+    height: 900,
+     icon: path.join(__dirname, 'build', 'high.ico')
   });
-  
+
   win.loadFile(
     path.join(__dirname, 'frontend', 'dist', 'index.html')
   );
- 
 }
 
 app.whenReady().then(() => {
 
-  backendProcess = spawn(
-    'node',
-    [path.join(__dirname, 'backend', 'index.js')],
-    {
-      stdio: 'inherit'
-    }
-  );
+  const backendPath = app.isPackaged
+    ? path.join(process.resourcesPath, 'backend', 'index.js')
+    : path.join(__dirname, 'backend', 'index.js');
+
+  backendProcess = fork(backendPath, [], {
+    silent: true
+  });
 
   setTimeout(() => {
     createWindow();
